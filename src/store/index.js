4561710dba8b4761d01c5704b6399b480aa136a3
gjyhuang <grace.jy.huang@gmail.com>
2020-03-06 22:00:07 -0500

    add: redux dependencies

---
+++ src/store/index.js
@@ -0,0 +1,21 @@
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+import { createLogger } from 'redux-logger';
+import thunkMiddleware from 'redux-thunk';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import user from './user';
+
+const middlewares = [thunkMiddleware];
+
+if (process.env.NODE_ENV === `development`) {
+  loggerMiddleware = createLogger({ collapsed: true });
+  middlewares.push(loggerMiddleware);
+}
+
+const reducer = combineReducers({ user, portfolio, transactions });
+const middleware = composeWithDevTools(
+  applyMiddleware(...middlewares)
+)
+const store = createStore(reducer, middleware);
+
+export default store;
+export * from './user';
