03cdaeec5e30d11436ab673318df7e582377b33f
gjyhuang <grace.jy.huang@gmail.com>
2020-03-11 19:04:11 -0400

    add: meta info

--- src/App.test.js
+++ src/App.test.js
@@ -2,8 +2,8 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders Hello World text', () => {
   const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const starterText = getByText(/hello world/i);
+  expect(starterText).toBeInTheDocument();
 });
